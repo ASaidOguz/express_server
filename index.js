198d64ddee7f02d1c23d7a283b286d4d8ca35bd1
ASaidOguz <ahmetsaid2136@gmail.com>
2023-01-05 16:26:34 +0300

    cors fix v1

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ initializePassport(passport,
 
 //Adding cors origin and setting credentials true to receiving connection...  
 app.use(cors({ 
-  origin:"http://localhost:3000",//<== location of the react app we r connecting!!
+  origin:"78.163.98.17:3000",//<== location of the react app we r connecting!!
   credentials:true
 }));
 app.use(express.json());
