6e68f20b29a0292c2b2f2e964f20be3a19db7dc2
ASaidOguz <ahmetsaid2136@gmail.com>
2023-01-07 01:24:18 +0300

    sss

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ initialize(passport,
 
 //Adding cors origin and setting credentials true to receiving connection...  
 app.use(cors({ 
-    origin:["*","https://vercel.com"],//<== location of the react app we r connecting!!
+    origin:["*"],//<== location of the react app we r connecting!!
     credentials:true, 
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
     exposedHeaders: ["set-cookie"],
