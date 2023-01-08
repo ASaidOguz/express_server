9b842c1c2755f5801ea8449cb887cfa1349daa70
ASaidOguz <ahmetsaid2136@gmail.com>
2023-01-08 21:25:55 +0300

    finalize

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var conString=process.env.CONSTRING
 var client= new pg.Client(conString)
 const jwt=require('jsonwebtoken')
 app.use(cors({
-  origin:"https://escrow-app-five.vercel.app/"
+  origin:"https://escrow-app-five.vercel.app"
 }))
 //Initilazing the database 
 client.connect(function(err){
